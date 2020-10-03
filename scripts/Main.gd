1785bb676295e879f886f86bfff1ccd1f2bcffbc
Ryan Stillwagon <stillwagonryan@gmail.com>
2020-10-02 17:20:46 -0700

    added: platform sprite

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -8,7 +8,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var path_parameters = Globals.get_path_radius()
+	print(path_parameters)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -4,10 +4,24 @@ extends Node
 var screen_width = 1024
 var screen_height = 600
 
-
-
-
+## play field variables
+var circle_field_start = 48
+var circle_field_end = 976
+var max_circle_space = 928
 
 
 func get_screen_size():
 	return Vector2(screen_width, screen_height)
+	
+func get_path_radius(size=32, num_sides=8):
+	print(size)
+	print(num_sides)
+	return Vector3(size / (2 *  sin(PI/num_sides)), size, num_sides)
+	
+func get_angle_size(num_sides):
+	return 360 / num_sides
+	
+func build_track(origin_point, radius, angle):
+	
+	
+	pass
